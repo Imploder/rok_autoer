5ef476358f59642aec1a17cc15a3e087a16a2f6a
Imploder <josh.lr.allen@gmail.com>
2019-05-26 13:56:45 +0100

    Add features to find images on screen and attack barbarians
    Find images:
    - Added methods to search for images on the screen
    - Added methods which try to find images for a certain time period
    - Added methods for clicking coordinates on the screen
    
    Attack barbarians:
    - Added method for navigating to the map
    - Added method to set the search level
    - Added method to search for barbarians
    - Added method to attack found barbarian

--- modules/barbarians.py
+++ modules/barbarians.py
@@ -17,16 +17,57 @@ class BarbarianCombat:
 
     def navigate_to_map(self):
         """Checks whether to and navigates to the map screen"""
-        screen_utilities = Utils()
-        location = screen_utilities.find('map')
-        if location is not None:
-            coordinates = (location.x, location.y)
-            screen_utilities.click(coordinates)
+        in_town = Utils.find_and_click('map')
+        if in_town:
+            Logger.log_message("success", "Navigated to map screen")
             self.on_map = True
         else:
-            on_map = screen_utilities.find('city')
-            if on_map is not None:
-                Logger.log_message("message", "Already on map screen")
+            on_map = Utils.find('city')
+            if on_map:
+                Logger.log_message("success", "Already on map")
                 self.on_map = True
             else:
                 Logger.log_message("error", "Move to the city or map screen")
+
+    def set_search_level(self):
+        """Resets the Barbarian Level to 1 and increments it to the desired level"""
+        Utils.wait_and_click('search')
+        Utils.wait_and_click('search_barbarian')
+
+        Utils.sleep(0.5)
+
+        minus_button = Utils.wait_and_find('search_minus', similarity=0.90)
+        if minus_button is not None:
+            Utils.sleep()
+            reset_bar_location = (minus_button.x + 45, minus_button.y + 12)
+            Utils.click(reset_bar_location)
+            Logger.log_message("success", "Reset search level")
+        else:
+            Logger.log_message("error", "could not find minus button")
+
+        plus_button = Utils.find('search_plus', similarity=0.90)
+        plus_button_location = (plus_button.x, plus_button.y)
+        for x in range(self.config.barbarian_level['BarbarianLevel'] - 1):
+            Utils.click(plus_button_location)
+
+    @staticmethod
+    def search_for_barbarian():
+        """Checks for/Navigates to the search button, and clicks it"""
+        search_button = Utils.find('search_search')
+        if search_button is not None:
+            search_button_location = (search_button.x, search_button.y)
+            Utils.click(search_button_location)
+        else:
+            Utils.wait_and_click('search')
+            Utils.wait_and_click('search_barbarian')
+            Utils.sleep(0.5)
+            search_button_location = (search_button.x, search_button.y)
+            Utils.click(search_button_location)
+
+    def attack_barbarian(self):
+        """Finds the created barbarian and attacks it with the default army"""
+        Utils.sleep()
+        Utils.wait_and_click(f"barbarian_{self.config.barbarian_level['BarbarianLevel']}", similarity=0.90)
+        Utils.wait_and_click('barbarian_attack')
+        Utils.wait_and_click('attack_new_troops')
+        Utils.wait_and_click('attack_march')
--- utils/screen.py
+++ utils/screen.py
@@ -1,7 +1,9 @@
 import numpy
 import pyautogui
 import cv2
-from utils.logger import Logger
+from datetime import datetime, timedelta
+from time import sleep
+from random import uniform
 
 
 class Region:
@@ -26,13 +28,34 @@ class Utils:
     """Tools to interact with and check the screen"""
 
     DEFAULT_SIMILARITY = 0.95
-    DEFAULT_WAIT = 2
+    DEFAULT_WAIT = 5
+
+    @staticmethod
+    def sleep(min_sleep=None, max_sleep=None):
+        """
+        Causes the script to wait for a set amount of time, useful for when wait methods fail
+        :param min_sleep: the minimum amount of time to wait
+        :param max_sleep: the maximum amount of time to wait
+        """
+        if min_sleep is None:
+            sleep(uniform(0.3, 0.5))
+        else:
+            max_sleep = min_sleep if max_sleep is None else max_sleep
+            sleep(uniform(min_sleep, max_sleep))
 
     @classmethod
     def update_screen(cls):
         """Takes a screen shot of the screen, converts it to be OpenCV readable and returns it"""
         image = pyautogui.screenshot(region=(0, 0, 1280, 720))
         return cv2.cvtColor(numpy.array(image), cv2.COLOR_RGB2GRAY)
+
+    @classmethod
+    def click(cls, coordinates):
+        """
+        Clicks at the given coordinates
+        :param coordinates: List of the x and y coordinate to click
+        """
+        pyautogui.click(coordinates[0], coordinates[1])
 
     @classmethod
     def find(cls, image: str, similarity=DEFAULT_SIMILARITY):
@@ -50,13 +73,51 @@ class Utils:
         value, location = cv2.minMaxLoc(result)[1], cv2.minMaxLoc(result)[3]
         if value >= similarity:
             return Region(location[0], location[1], width, height)
-        Logger.log_message("error", f"Could not find {image}.png in screen shot")
         return None
 
     @classmethod
-    def click(cls, coordinates):
+    def find_and_click(cls, image, similarity=DEFAULT_SIMILARITY):
         """
-        Clicks at the given coordinates
-        :param coordinates: List of the x and y coordinate to click
+        Finds an image on the screen and clicks it
+        :param image: The image to find
+        :param similarity: How much in percent the image should match
+        :return: True if image was found, false if not
         """
-        pyautogui.click(coordinates[0], coordinates[1])
+        region = cls.find(image, similarity)
+        if region is not None:
+            coordinates = (region.x, region.y)
+            cls.click(coordinates)
+            return True
+        return False
+
+    @classmethod
+    def wait_and_find(cls, image, wait=DEFAULT_WAIT, similarity=DEFAULT_SIMILARITY):
+        """
+        Continue to try to find the image for a set amount of time
+        :param image: The image to find
+        :param wait: The time in seconds to try to find the image
+        :param similarity: How much in percent the image should match
+        :return: A region class with coordinate variables or None if image not found
+        """
+        wait_time = datetime.now() + timedelta(seconds=wait)
+        while datetime.now() < wait_time:
+            region = cls.find(image, similarity)
+            if region is not None:
+                return region
+        return None
+
+    @classmethod
+    def wait_and_click(cls, image, wait=DEFAULT_WAIT, similarity=DEFAULT_SIMILARITY):
+        """
+        Continue to try to find the image for a set amount of time and then click it
+        :param image: The image to find
+        :param wait: The time in seconds to try to find the image
+        :param similarity: How much in percent the image should match
+        :return: True if image was found, false if not
+        """
+        wait_time = datetime.now() + timedelta(seconds=wait)
+        while datetime.now() < wait_time:
+            if cls.find_and_click(image, similarity):
+                return True
+        return False
+
